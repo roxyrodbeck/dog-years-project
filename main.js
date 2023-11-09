735ee64544109edc29f0490bf2d2a59ab8aca1eb
Roxana Rodriguez-Becker <126708010+roxyrodbeck@users.noreply.github.com>
2023-11-08 18:15:37 -0600

    Create main.js
    Uploaded dog years project from #dev30

---
+++ main.js
@@ -0,0 +1,19 @@
+// this represents my age, unfortunately
+var myAge = 40;
+
+// this value will change
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+// taking away the 2 we included in earlyYears
+let laterYears = myAge - 2;
+laterYears *= 4;
+
+myAgeInDogYears = earlyYears + laterYears;
+
+// is this the bulit in method? 
+let myName = 'Roxy';
+
+console.log(myName.toLowerCase());
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
