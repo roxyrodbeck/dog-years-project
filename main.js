3c552cecc710b4db181267c155d96739a6b59677
Roxana Rodriguez-Becker <126708010+roxyrodbeck@users.noreply.github.com>
2023-11-09 13:04:14 -0600

    Update main.js
    Updated after viewing dev30 "answer" video.

--- main.js
+++ main.js
@@ -9,11 +9,12 @@ earlyYears *= 10.5;
 let laterYears = myAge - 2;
 laterYears *= 4;
 
+// combining early and later years
 myAgeInDogYears = earlyYears + laterYears;
 
-// is this the bulit in method? 
-let myName = 'Roxy';
+// edited to adjust to dev30 answer 
+const myName = 'Roxy'.toLowerCase();
 
-console.log(myName.toLowerCase());
+// console.log(myName.toLowerCase());
 
 console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
